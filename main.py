297698be31db1661ea98c60d213cdf5a0b52c9f9
Agathe <agathe.plu@student-cs.fr>
2023-10-11 19:15:43 +0200

    Do the analysis of correlation between the features of interest and the other features

--- main.py
+++ main.py
@@ -4,4 +4,29 @@ Main Python module launching the pipeline to assess the delay of the TGV.
 Functions
 ---------
 """
-from tools.tools_database import Read_data 
+
+###############
+### Imports ###
+###############
+
+### Module imports ###
+
+from tools.tools_analysis import (
+    display_correlation_graph
+)
+from tools.tools_constants import (
+    PATH_DATASET
+)
+from tools.tools_database import (
+    Read_data
+)
+
+#################
+### Main code ###
+#################
+
+dataset = Read_data(PATH_DATASET)
+
+### Analysis of the correlation with features of interest ###
+
+display_correlation_graph(dataset)
