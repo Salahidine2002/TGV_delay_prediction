75703f2e555ab1b72cc47801983370d6a8af263b
Laure-Emilie <laure-emilie.martin@student-cs.fr>
2023-10-13 10:21:45 +0200

    import of some regressors

--- tools/tools_models.py
+++ tools/tools_models.py
@@ -4,3 +4,19 @@ Python module to train differents architectures on the train data.
 Functions
 ---------
 """
+
+from sklearn.ensemble import (
+    GradientBoostingRegressor,
+    HistGradientBoostingRegressor,
+    RandomForestRegressor
+)
+
+from sklearn.tree import (
+    DecisionTreeRegressor
+)
+
+
+
+
+
+
