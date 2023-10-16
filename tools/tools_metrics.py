04639314643519d5f62b39c226e1022aafaeb37f
Ramdane Ibrahim <ibrahim.ramdane@student-cs.fr>
2023-10-16 12:11:32 +0200

    ajout accuracy

--- tools/tools_metrics.py
+++ tools/tools_metrics.py
@@ -4,6 +4,25 @@ Python module containing the metrics to evaluate the results of the models.
 Functions
 ---------
 """
+from sklearn.metrics import accuracy_score
 
-def accuracy():
-    pass
+
+def accuracy(y_test, y_predicted):
+    """
+    Compute accuracy_score
+
+    Parameters
+    ----------
+    y_test :
+        Feature to predict
+
+    y_predicted :
+        Feature predicted
+
+    Returns
+    -------
+    accuracy_score : float
+        accurary of the model
+    """
+
+    return accuracy_score(y_test, y_predicted)
